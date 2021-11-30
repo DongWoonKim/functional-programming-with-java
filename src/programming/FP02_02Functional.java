39e579a2a2bb9756d78a232fdbdf032739a3fc6b
DongwoonKim <01030985843z@naver.com>
2021-11-30 18:16:13 +0900

    Functional Programming - sorted, collect

--- src/programming/FP02_02Functional.java
+++ src/programming/FP02_02Functional.java
@@ -1,17 +1,27 @@
 package programming;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP02_02Functional {
 
     public static void main(String[] args) {
         List<Integer> numbers =  List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
-
+        List<Integer> doubledNumbers = doubleList( numbers );
+        List<String> words = List.of("Spring", "Spring Boot", "API" , "Microservices", "AWS", "PCF","Azure", "Docker", "Kubernetes");
+        /*
         int sum = addListFunctinal( numbers );
         System.out.println(sum);
         sum = addListFunctinal2(numbers);
         System.out.println(sum);
+        removeDuplicateFunctional(numbers);
+        sortedFunctional(numbers);
+        */
 
+        sortedWordsFunctional(words);
+        sortedWordsFunctional2(words);
+        for ( int num : doubledNumbers ) System.out.println(num);
     }
 
     private static int sum( int aggregate, int nextNumber ) {
@@ -33,5 +43,45 @@ public class FP02_02Functional {
 //                .reduce(0, (a, b) -> a + b);
                 .reduce(0, Integer::sum);
     }
+
+    private static void removeDuplicateFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
+
+    private static void sortedFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void sortedWordsFunctional(List<String> str) {
+        str.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+    private static void sortedWordsFunctional2(List<String> str) {
+
+        // 역정렬
+        str.stream()
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
+        System.out.println();
+
+        // 문자열 길이 기준으로 정렬
+        str.stream()
+                .sorted(Comparator.comparing( s -> s.length() ))
+                .forEach(System.out::println);
+    }
+
+    private static List<Integer> doubleList(List<Integer> numbers) {
+        return numbers.stream()
+                .map(x -> x * x)
+                .collect(Collectors.toList());
+    }
+
 
 }
--- src/programming/FP02_03Exercise.java
+++ src/programming/FP02_03Exercise.java
@@ -9,14 +9,22 @@ Cube every number in a list and find the sum of cubes
 
 Exercise 9
 Find Sum of Odd Numbers in a list
+
+Exercise 10
+Create a List with Even Numbers Filtered from the Numbers List
+
+Exercise 11
+Create a List with lengths of all course titles.
  */
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP02_03Exercise {
 
     public static void main(String[] args) {
 
+        List<String> words = List.of("Spring", "Spring Boot", "API" , "Microservices", "AWS", "PCF","Azure", "Docker", "Kubernetes");
         List<Integer> numbers =  List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
         int ret = Exercise7(numbers);
         System.out.println(ret);
@@ -27,6 +35,12 @@ public class FP02_03Exercise {
         ret = Exercise9(numbers);
         System.out.println(ret);
 
+        List<Integer> getList = Exercise10(numbers);
+        for ( int num : getList ) System.out.println(num);
+
+        System.out.println();
+        List<Integer> getStrLenList = Exercise11(words);
+        for ( int num : getStrLenList ) System.out.println(num);
     }
 
     /*
@@ -60,6 +74,26 @@ public class FP02_03Exercise {
         return numbers.stream()
                 .filter(x -> x % 2 == 1)
                 .reduce(0, Integer::sum);
+    }
+
+    /*
+    Exercise 10
+    Create a List with Even Numbers Filtered from the Numbers List
+    */
+    private static List<Integer> Exercise10(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(x -> x % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
+    /*
+    Exercise 11
+    Create a List with lengths of all course titles.
+    */
+    private static List<Integer> Exercise11(List<String> str) {
+        return str.stream()
+                .map( s -> s.length() )
+                .collect(Collectors.toList());
     }
 
 }
